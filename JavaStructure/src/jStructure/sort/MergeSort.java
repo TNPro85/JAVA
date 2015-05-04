e48a05b0ce1fb8d4d33a944c036b4e3166937ae9
tuannh <mrhoangtuan@gmail.com>
2015-05-05 03:14:55 +0700

    - update merge sort

--- JavaStructure/src/jStructure/sort/MergeSort.java
+++ JavaStructure/src/jStructure/sort/MergeSort.java
@@ -3,31 +3,72 @@ package jStructure.sort;
 
 public class MergeSort {
 
-	public static void selectionSort(int arr[], int size) {
-		for(int i = 0; i < size - 1; i++) {
-			// Gia su phan tu 0 la phan tu nho nhat
-			int min = i;
-			for(int j = i+1; j < size; j++) {
-				if(arr[j] < arr[min])
-					min = j;
-			}
-			
-			if(min != i) {
-				arr[i] = arr[i] ^ arr[min];
-				arr[min] = arr[i] ^ arr[min];
-				arr[i] = arr[i] ^ arr[min];
-			}
+	private int[] numbers;
+	private int[] helper;
+
+	private int size;
+
+	public void mergeSort(int[] values) {
+		this.numbers = values;
+		size = values.length;
+		this.helper = new int[size];
+		mergesort(0, size - 1);
+	}
+
+	private void mergesort(int low, int high) {
+		// check if low is smaller then high, if not then the array is sorted
+		if (low < high) {
+			// Get the index of the element which is in the middle
+			int middle = low + (high - low) / 2;
+			// Sort the left side of the array
+			mergesort(low, middle);
+			// Sort the right side of the array
+			mergesort(middle + 1, high);
+			// Combine them both
+			merge(low, middle, high);
 		}
-		
-		for(int i = 0; i < size; i++) {
+	}
+
+	private void merge(int low, int middle, int high) {
+
+		// Copy both parts into the helper array
+		for (int i = low; i <= high; i++) {
+			helper[i] = numbers[i];
+		}
+
+		int i = low;
+		int j = middle + 1;
+		int k = low;
+		// Copy the smallest values from either the left or the right side back
+		// to the original array
+		while (i <= middle && j <= high) {
+			if (helper[i] <= helper[j]) {
+				numbers[k] = helper[i];
+				i++;
+			} else {
+				numbers[k] = helper[j];
+				j++;
+			}
+			k++;
+		}
+		// Copy the rest of the left side of the array into the target array
+		while (i <= middle) {
+			numbers[k] = helper[i];
+			k++;
+			i++;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		int arr[] = {40, 2, 1, 43, 3, 65, 0, -1, 58, 3, 42, 4};
+
+		MergeSort mergeSort = new MergeSort();
+		mergeSort.mergeSort(arr);
+
+		for(int i = 0; i < arr.length; i++) {
 			System.out.print(arr[i] + " ");
 		}
 		System.out.println("");
-	}
-	
-	public static void main(String[] args) {
-		int arr[] = {40, 2, 1, 43, 3, 65, 0, -1, 58, 3, 42, 4};
-		int size = 12;
-		selectionSort(arr, size);
 	}
 }
